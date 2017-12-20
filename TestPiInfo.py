c66794e3ac8026498962459b18821d860c7c98d2
ARosman77 <ales.rosman@gmail.com>
2017-12-20 12:35:35 +0100

    Test for Issue #3.

--- TestPiInfo.py
+++ TestPiInfo.py
@@ -6,35 +6,20 @@ import piInfo.sonarr
 import piInfo.system
 import requests
 
-print(piInfo.temp.CPU())
-print(str(piInfo.sonarr.diskSpace('media'))+'% free.')
-episodes = piInfo.sonarr.upcomingEp(7)
-for ep in episodes:
-    print(str(ep["seriesTitle"]))
-
-#time = piInfo.system.upTime()
-# test limits
-#from datetime import timedelta
-#time = timedelta(0,134*3600+43*60) 
-#print(time)
-
-# show (xx min) until 59 min
-#mode1Limit = 59*60
-# show (xxx:xx) until 999h 59 min
-#mode2Limit = 999*3600 + 59*60
-# show (xxxxxh) else
-
-#upTime=int(time.total_seconds()//60)
-#upTime = int(time.total_seconds())
-#if upTime <= mode1Limit:
-#    upTimeStr = str(upTime//60)+' min'
-#elif upTime <= mode2Limit:
-#    upTimeStr = '{:0>2}'.format(str(upTime//3600)) \
-#       + ':'+ '{:0>2}'.format(str((upTime%3600)//60))
-#else:
-#    upTimeStr = str(upTime//3600)+'h'
-#
-#upTimeStr = '{:^6}'.format(upTimeStr)
-
+# test temperature readings
+print("CPU temperature:" + str(piInfo.temp.CPU()) + 'C')
+# test free disk space readings
+print("Disk "+str(piInfo.sonarr.diskSpace('media'))+'% free.')
+# test upTime for display command
 print(piInfo.system.upTimeAsDispStr())
+# test upcomming episodes list
+episodes = piInfo.sonarr.upcomingEp(7)
+for idx,ep in enumerate(episodes):
+    seriesTitle = '{:15}'.format(ep["seriesTitle"][:15])
+    epNum = 'E'+'{:0>2}'.format(str(ep["episodeNumber"]))
+    if ep["deltaDays"].days >=0:
+        deltaDays = '+'+str(ep["deltaDays"].days)+'-'
+    else:
+        deltaDays = str(ep["deltaDays"].days)+'-'
+    print(deltaDays+seriesTitle+epNum)
 
