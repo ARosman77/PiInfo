35cc949fcb08820f63a09562f32b6ce4678601f9
ARosman77 <ales.rosman@gmail.com>
2017-12-20 10:54:48 +0100

    New function written in system part that returns the string that can be displayed on the oled display. Closes #1

--- TestPiInfo.py
+++ TestPiInfo.py
@@ -12,29 +12,29 @@ episodes = piInfo.sonarr.upcomingEp(7)
 for ep in episodes:
     print(str(ep["seriesTitle"]))
 
-time = piInfo.system.upTime()
+#time = piInfo.system.upTime()
 # test limits
 #from datetime import timedelta
 #time = timedelta(0,134*3600+43*60) 
 #print(time)
 
 # show (xx min) until 59 min
-mode1Limit = 59*60
+#mode1Limit = 59*60
 # show (xxx:xx) until 999h 59 min
-mode2Limit = 999*3600 + 59*60
+#mode2Limit = 999*3600 + 59*60
 # show (xxxxxh) else
 
 #upTime=int(time.total_seconds()//60)
-upTime = int(time.total_seconds())
-if upTime <= mode1Limit:
-    upTimeStr = str(upTime//60)+' min'
-elif upTime <= mode2Limit:
-    upTimeStr = '{:0>2}'.format(str(upTime//3600)) \
-       + ':'+ '{:0>2}'.format(str((upTime%3600)//60))
-else:
-    upTimeStr = str(upTime//3600)+'h'
+#upTime = int(time.total_seconds())
+#if upTime <= mode1Limit:
+#    upTimeStr = str(upTime//60)+' min'
+#elif upTime <= mode2Limit:
+#    upTimeStr = '{:0>2}'.format(str(upTime//3600)) \
+#       + ':'+ '{:0>2}'.format(str((upTime%3600)//60))
+#else:
+#    upTimeStr = str(upTime//3600)+'h'
+#
+#upTimeStr = '{:^6}'.format(upTimeStr)
 
-upTimeStr = '{:^6}'.format(upTimeStr)
-
-print(upTimeStr)
+print(piInfo.system.upTimeAsDispStr())
 
--- piInfo/system.py
+++ piInfo/system.py
@@ -7,3 +7,20 @@ def upTime():
         uptime_seconds = float(f.readline().split()[0])
         result = timedelta(seconds = uptime_seconds)
     return result
+
+def upTimeAsDispStr():
+    # show (xx min) until 59 min
+    mode1Limit = 59*60
+    # show (xxx:xx) until 999h 59 min
+    mode2Limit = 999*3600 + 59*60
+    # show (xxxxxh) else
+    time = int(upTime().total_seconds())
+    if time <= mode1Limit:
+        upTimeStr = str(time//60)+' min'
+    elif time <= mode2Limit:
+        upTimeStr = '{:0>2}'.format(str(time//3600)) \
+            + ':'+ '{:0>2}'.format(str((time%3600)//60))
+    else:
+        upTimeStr = str(time//3600)+'h'
+    upTimeStr = '{:^6}'.format(upTimeStr)
+    return upTimeStr
